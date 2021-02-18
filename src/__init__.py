e19df610bf9996c9e7c938226869b3e7ee10946a
Suferas <69329902+Suferas@users.noreply.github.com>
2021-02-18 00:17:47 -0300

    Add files via upload

---
+++ src/__init__.py
@@ -0,0 +1,53 @@
+from polynomial_interpolator import operators, polynomial_interpolator
+from sympy import *
+import numpy as np
+from matplotlib import pyplot as plt
+
+set_of_x = []
+set_of_y = []
+
+
+number_of_elements = int(input("Number of elements: "))
+
+for i in range(number_of_elements):
+     if i == 0:
+          elements_of_x = input('Set of x: \n')
+          
+          set_of_x.append(elements_of_x)
+     else:
+          elements_of_x = input()
+          
+          set_of_x.append(elements_of_x)
+for i in range(number_of_elements):
+     if i == 0:
+          elements_of_y = input('Set of f(x):\n')
+                    
+          set_of_y.append(elements_of_y)
+
+     else:
+          elements_of_y = input()
+          set_of_y.append(elements_of_y)
+
+
+y = polynomial_interpolator(set_of_x, set_of_y)
+print('\nPolynom:\n '+str(y))
+
+
+x = symbols('x')
+f_x = lambdify(x, y, modules=['numpy'])
+
+x_values = np.linspace(int(min(set_of_x))-100, int(max(set_of_x))+100, 500)
+y_values = f_x(x_values)
+
+fig, ax = plt.subplots()
+
+ax.plot(x_values, y_values, label = 'y = '+str(y))
+ax.axhline(y=0, color='k')
+ax.axvline(x=0, color='k')
+
+plt.title('Function graph')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.grid(alpha=.4, linestyle = '-')
+plt.legend(prop = {'size':6})
+plt.show()
---
+++ src/polynomial_interpolator.py
@@ -0,0 +1,78 @@
+import numpy as np
+import sympy as sp
+from sympy import *
+import matplotlib.pyplot as plt
+
+sp.init_printing()
+
+
+def operators(x, y):
+    delta_y = []
+    for a in range(len(x)):
+        delta_y.append(y[a])
+
+    for i in range(1, len(x)):
+
+        for j in range(len(x)-1, i-1, -1):
+            delta_y = [float(k) for k in delta_y]
+            delta_y[j] = (delta_y[j]-delta_y[j-1])/(x[j]-x[j-i])
+    return  delta_y
+
+          
+
+def polynomial_interpolator(set_of_x, set_of_y):
+     set_of_x = [float(i) for i in set_of_x]
+     set_of_y = [float(i) for i in set_of_y]
+     set_of_operators = []
+     set_of_operators = [float(i) for i in set_of_operators]
+     set_of_operators.extend(operators(set_of_x, set_of_y)) 
+     x = symbols('x') 
+     
+     def diferences_of_x(m):
+
+          if m == 0:
+               diference_of_x = 1
+                         
+          else:
+               diference_of_x = x - set_of_x[m-1]
+
+          return diference_of_x
+
+     def recursive_diferences_of_x(q):
+
+          set_of_diferences = []
+          while q >= 0:
+               if q == 0:
+                    recursive_diference_of_x = 1
+               else:
+                    recursive_diference_of_x = diferences_of_x(q)*diferences_of_x(q-1)
+
+               set_of_diferences.append(recursive_diference_of_x)
+                    
+               q-=2
+
+          diferences = np.prod(set_of_diferences)
+
+          return diferences
+
+     def variable(i):
+          variable = set_of_operators[i]*recursive_diferences_of_x(i)
+          return variable
+
+     set_of_variables = []
+     for f in range(0, len(set_of_x), 2):
+
+          if f == len(set_of_x)-1:
+               recursive_variables = variable(f)
+          
+          else:
+               recursive_variables = variable(f) + variable(f+1)
+
+          
+          set_of_variables.append(recursive_variables)
+
+     
+     sum_of_variables = sum(set_of_variables) 
+     polynom = simplify(sum_of_variables, rational = True)
+
+     return polynom
